47579b0c52cfe6fa1fff63f1c928aef311a35260
Matt Johnson <grillpanda@gmail.com>
2013-05-25 10:37:53 +0100

    Add epubcheck step to rebuild script.

--- epub/rebuild.sh
+++ epub/rebuild.sh
@@ -4,3 +4,4 @@ rm Synth\ Secrets.epub
 zip -X Synth\ Secrets.epub mimetype
 zip -rg Synth\ Secrets.epub META-INF -x \*.DS_Store
 zip -rg Synth\ Secrets.epub OPS -x \*.DS_Store
+epubcheck Synth\ Secrets.epub
